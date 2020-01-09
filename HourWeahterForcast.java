fd174503aed506786fd9b2141e439224b10a3a5e
tlfolkman <44623133+tlfolkman@users.noreply.github.com>
2020-01-08 20:09:42 -0700

    HourWeatherForecast Class

---
+++ HourWeahterForcast.java
@@ -0,0 +1,11 @@
+package com.example.weatherapp;;
+
+import java.util.List;
+
+public class HourWeahterForcast {
+    List<WeatherForcast>list;
+
+    public String toString() {
+        return list.toString();
+    }
+}
